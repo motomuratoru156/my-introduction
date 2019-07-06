23e012e99a881036695fd8239eafabd70b32bd99
motomura <motomura.miraidenshi@gmail.com>
2019-07-06 13:00:14 +0900

    add new files

---
+++ my-introduction.js
@@ -0,0 +1,16 @@
+$(function(){
+    
+    $('.btn-question').click(function(){
+        $('.modal').fadeIn();
+    });
+
+
+    $('.btn-close').click(function(){
+        $('.modal').fadeOut();
+      });
+
+    $('.baseball').click(function(){
+        $('.').fadeIn();
+    });
+
+});
